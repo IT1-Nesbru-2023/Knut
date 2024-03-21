0f62a65c6d6606ff2eab7a7bae55a5843dc494d3
Knutoh <145427375+Knutoh@users.noreply.github.com>
2024-03-21 14:32:54 +0100

    Add files via upload

---
+++ jsonvisittkort.js
@@ -0,0 +1,5 @@
+let visittKort = {"kortArray":[
+    {"navn":"Ole Olsen","tlf":"har ikke", "adr":"almedalsveien 21","stilling":"Rektor","kjønn":"Mann","født":"01.03.1987","førerkort":"ja"},
+    {"navn":"Per Person","tlf":"41544773", "adr":"kruttveien 8","stilling":"Daglig leder","kjønn":"Mann","født":"06.09.1990","førerkort":"ja"},
+    {"navn":"Helge Helgesen" ,"tlf":"29674939", "adr":"trollgata 5","stilling":"Vaktmester","kjønn":"Kvinne","født":"08.05.1940","førerkort":"nei"},
+    {"navn":"Nissa Nyberget" ,"tlf":"46527563", "adr":"eventyrveien 2","stilling":"Lærer","kjønn":"Kvinne","født":"19.09.1985","førerkort":"ja"}]}
